4a749272c2ab6f3135fd82be6ce1690056cb7893
Fdvky1 <fdvkey@gmail.com>
2023-11-28 22:57:59 +0700

    fix: log

--- pages/Dashboard/index.vue
+++ pages/Dashboard/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="container mx-auto px-4 pt-4">
-            <div class="mockup-code">
+            <div class="mockup-code bg-neutral-content text-black dark:bg-base-100 dark:text-slate-100">
                 <div class="h-[30vh] lg:h-[22vh] overflow-y-auto" ref="logsContainer">
                     <pre data-prefix="~" v-for="(message, index) in botState.logs" :key="index"><code>{{ message }}</code></pre>
                 </div>
@@ -24,8 +24,8 @@ const logsContainer = ref<HTMLDivElement | null>(null)
 
 let socket: WebSocket
 
-onMounted(()=> {
-    botState.fetchStatus(token.value!)
+onMounted(async()=> {
+    await botState.fetchStatus(token.value!)
     socket = new WebSocket(runtimeConfig.public.serviceUrl!.replace("http", "ws") + "/ws?token=" + token.value)
     
     socket.addEventListener("open", () => {
